7988812fcf44525f97db4ef5a9e5987cce70bf6c
boom <boomtanut@gmail.com>
2023-11-14 12:24:04 +0700

    add table control

--- controllers/table.controller.js
+++ controllers/table.controller.js
@@ -1,9 +1,136 @@
-
+import {client} from '../grpc/grpc.js'
 export const GetTables = async (req, res, next) => {
   try {
-    res.status(200).json({
-        success: true,
-    })
+    client.GetTables(null,(err,data)=>{
+      if(err) throw err;
+      res.status(200).json({
+        success:true ,
+        data: data
+      })
+  });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+    console.log(err.stack);
+  }
+};
+
+export const BookSeat = async (req, res, next) => {
+  try {
+    console.log(req.user.id)
+    console.log(req.body)
+    const payload = {
+      userId : String(req.user.id), 
+      tableId : req.body.tableId,
+    }
+    
+    client.BookSeat(payload,(err,data)=>{
+      if(err) throw err;
+      res.status(200).json({
+        success:true ,
+        data: data
+      })
+  });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+    console.log(err.stack);
+  }
+};
+
+export const CancelSeat = async (req, res, next) => {
+  const payload = {
+    userId : String(req.user.id), 
+    tableId : req.body.tableId,
+  }
+  try {
+    client.CancelSeat(payload,(err,data)=>{
+      if(err) throw err;
+      res.status(200).json({
+        success:true ,
+        data: data
+      })
+  });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+    console.log(err.stack);
+  }
+};
+
+export const CheckIn = async (req, res, next) => {
+  const payload = {
+    userId : String(req.user.id), 
+    tableId : req.body.tableId,
+  }
+  try {
+    client.CheckIn(payload,(err,data)=>{
+      if(err) throw err;
+      res.status(200).json({
+        success:true ,
+        data: data
+      })
+  });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+    console.log(err.stack);
+  }
+};
+
+export const VerifyCheckIn = async (req, res, next) => {
+  const payload = {
+    requestId : req.body.requestId
+  }
+  try {
+    client.VerifyCheckIn(payload,(err,data)=>{
+      if(err) throw err;
+      res.status(200).json({
+        success:true ,
+        data: data
+      })
+  });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+    console.log(err.stack);
+  }
+};
+
+export const CheckOut = async (req, res, next) => {
+  const payload = {
+    tableId : req.body.tableId
+  }
+  try {
+    client.CheckOut(payload,(err,data)=>{
+      if(err) throw err;
+      res.status(200).json({
+        success:true ,
+        data: data
+      })
+  });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+    console.log(err.stack);
+  }
+};
+
+export const DeleteLateTables = async (req, res, next) => {
+  try {
+    client.DeleteLateTables(null,(err,data)=>{
+      if(err) throw err;
+      res.status(200).json({
+        success:true ,
+        data: data
+      })
+  });
   } catch (err) {
     res.status(400).json({
       success: false,
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import config from "./config/config.js"
 main()
 
 async function main(){
+
+
     app.listen(config.server_port || 3006, config.server_url || "localhost",()=>{
         console.log(`🚀 Server has listening on ${config.server_url || "localhost"}:${config.server_port || 3000}`)
     })
